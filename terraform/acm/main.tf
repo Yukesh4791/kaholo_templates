0afce373ac00d1404859ae7a62006ed5fcafd37b
yukesh4791 <yukesh4791@gmail.com>
2022-02-01 16:34:58 +0100

    fix(acm): added domain_name var

--- terraform/acm/main.tf
+++ terraform/acm/main.tf
@@ -2,37 +2,41 @@ locals {
   # Use existing (via data source) or create new zone (will fail validation, if zone is not reachable)
   use_existing_route53_zone = true
 
-  domain_name = "example.com"
+#  domain_name = "example.com"
 }
 
+variable "domain_name" {
+  type        = string
+  description = "Domain name"
+}
 data "aws_route53_zone" "this" {
   count = local.use_existing_route53_zone ? 1 : 0
 
-  name         = local.domain_name
+  name         = var.domain_name
   private_zone = false
 }
 
 resource "aws_route53_zone" "this" {
   count = !local.use_existing_route53_zone ? 1 : 0
-  name  = local.domain_name
+  name  = var.domain_name
 }
 
 module "acm" {
   source = "terraform-aws-modules/acm/aws"
 
-  domain_name = local.domain_name
+  domain_name = var.domain_name
   zone_id     = coalescelist(data.aws_route53_zone.this.*.zone_id, aws_route53_zone.this.*.zone_id)[0]
 
   subject_alternative_names = [
-    "*.alerts.${local.domain_name}",
-    "new.sub.${local.domain_name}",
-    "*.${local.domain_name}",
-    "alerts.${local.domain_name}",
+    "*.alerts.${var.domain_name}",
+    "new.sub.${var.domain_name}",
+    "*.${var.domain_name}",
+    "alerts.${var.domain_name}",
   ]
 
   wait_for_validation = true
 
   tags = {
-    Name = local.domain_name
+    Name = var.domain_name
   }
 }

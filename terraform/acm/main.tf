68f1bd0b29bbabddfa2180d0f305c333e5897813
yukesh4791 <yukesh4791@gmail.com>
2022-02-11 12:05:25 +0100

    fix(acm): added access and secret key to provider

--- terraform/acm/main.tf
+++ terraform/acm/main.tf
@@ -3,10 +3,6 @@ locals {
   use_existing_route53_zone = true
 }
 
-variable "domain_name" {
-  type        = string
-  description = "Domain name"
-}
 data "aws_route53_zone" "this" {
   count = local.use_existing_route53_zone ? 1 : 0
 
@@ -21,7 +17,7 @@ resource "aws_route53_zone" "this" {
 
 module "acm" {
   source = "terraform-aws-modules/acm/aws"
-
+  version = "v3.3.0"
   domain_name = var.domain_name
   zone_id     = coalescelist(data.aws_route53_zone.this.*.zone_id, aws_route53_zone.this.*.zone_id)[0]
 
---
+++ terraform/acm/variables.tf
@@ -0,0 +1,14 @@
+variable "domain_name" {
+  type        = string
+  description = "Domain name"
+}
+
+variable "access_key" {
+  type        = string
+  description = "Access Key"
+}
+
+variable "secret_key" {
+  type        = string
+  description = "Secret Key"
+}
